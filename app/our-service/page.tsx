ecae2cba3751d983695371b6f3f20ccb00fcb5b1
nalendramuhammad <itsnalendraa@gmail.com>
2025-04-09 17:14:25 +0700

    our service page

--- app/our-service/page.tsx
+++ app/our-service/page.tsx
@@ -2,8 +2,40 @@
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Service</h1>
-    </div>
+    <>
+      <section className="service-wrap">
+        <div className="service-overlay"></div>
+        <div className="service-content">
+          <h1>Services That Empower Your Business</h1>
+        </div>
+      </section>
+      <section className="our-service-section">
+        <div className="our-service-section-header">
+          <div className="our-service-section-title-wrap">
+            <span className="our-service-section-subtitle">Our Service</span>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+        <div className="our-service-section-grid">
+          {Array(6)
+            .fill(0)
+            .map((_, index) => (
+              <div
+                key={index}
+                className="our-service-section-item"
+              >
+                <img
+                  src="/image/placeholder.png"
+                  alt={`Industry ${index + 1}`}
+                />
+              </div>
+            ))}
+        </div>
+      </section>
+    </>
   );
 }
