c361fe4a6f852928e58abdf2e32ee513376161ad
nalendra <94297761+nalendramuhammad@users.noreply.github.com>
2025-06-08 01:30:29 +0700

    update

--- app/(public)/our-partner/page.tsx
+++ app/(public)/our-partner/page.tsx
@@ -2,9 +2,19 @@
 
 import { useTranslation } from "react-i18next";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function PartnerPage() {
   const { t } = useTranslation();
+  const [partnerImages, setPartnerImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetch("/data/partners.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setPartnerImages([...(data.partners || []), ...(data.partners2 || [])]);
+      });
+  }, []);
 
   return (
     <>
@@ -21,6 +31,21 @@ export default function PartnerPage() {
             width={30}
             height={30}
           />
+        </div>
+      </section>
+      <section className="partner-list-section">
+        <h2 className="partner-list-heading">Our Partner</h2>
+        <div className="partner-list-grid">
+          {partnerImages.map((src, idx) => (
+            <Image
+              key={idx}
+              src={src}
+              alt={`Partner ${idx + 1}`}
+              width={200}
+              height={100}
+              className="partner-list-img"
+            />
+          ))}
         </div>
       </section>
     </>
